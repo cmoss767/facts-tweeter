ec8153044c8a679c96d88a01f2997089316950b5
Chris Moss <hcmoss70@gmail.com>
2023-12-11 13:16:12 -0500

    runs on cli now

--- postTweet.js
+++ postTweet.js
@@ -1,4 +1,3 @@
-import express from 'express'
 import { TwitterApi } from 'twitter-api-v2'
 import axios from 'axios'
 import 'dotenv/config'
@@ -10,25 +9,17 @@ const client = new TwitterApi({
   accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 })
 
-const app = express()
-
-app.post('/twitter', async (req, res) => {
+const postTweet = async () => {
   try {
     const { data } = await axios.get(process.env.FACTS_API_URl, {
       headers: {
         'X-Api-Key': process.env.FACTS_API_KEY,
       },
     })
-    const tweet = await client.v2.tweet(data[0].fact)
-    return res.json({ message: 'tweet was posted' })
+    await client.v2.tweet(data[0].fact)
+    console.log('tweet was posted')
   } catch (error) {
-    return res.status(500).json({ message: 'error posting tweet' })
+    console.log(error)
   }
-})
-app.get('/', (_req, res) => {
-  return res.json({ message: 'app is running' })
-})
-
-app.listen(5003, () => {
-  console.log('server is running on port 5003')
-})
+}
+postTweet()
